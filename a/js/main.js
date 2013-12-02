c36213c52b66c7e8ad3222237c5d5e5121d37a3c
Pascal Laliberté <pascal.laliberte@gmail.com>
2013-12-01 21:53:47 -0500

    Fixed showing the .intro when top-gutter is not shown, after a resize - Added running the code to evaluate the page properties on resize, in addition to on scroll

--- a/js/main.js
+++ a/js/main.js
@@ -5,6 +5,32 @@
 			return -c * ((t=t/d-1)*t*t*t - 1) + b;
 		}
   });
+
+  update_page_properties = function(){
+    scrollPos = $(window).scrollTop();
+    topGutterHeight = parseInt($("body").eq(0).css("padding-top"))
+
+    if (0 >= scrollPos) {
+      $("body").eq(0).addClass('is-top');
+      $("body").eq(0).removeClass('is-not-top');
+    } else {
+      $("body").eq(0).removeClass('is-top');
+      $("body").eq(0).addClass('is-not-top');
+    }
+
+    setTimeout(function(){
+      scrollPos = $(window).scrollTop();
+      
+      if (scrollPos < (topGutterHeight - 31)) {
+        $("body").eq(0).addClass('is-top-gutter-shown');
+        $("body").eq(0).removeClass('is-not-top-gutter-shown');
+      } else {
+        $("body").eq(0).removeClass('is-top-gutter-shown');
+        $("body").eq(0).addClass('is-not-top-gutter-shown');
+      }
+    }, 500);
+  }
+
   
   $(document).ready(function(){
     $("body").eq(0).addClass('js');
@@ -19,36 +45,13 @@
     $(".site").eq(0).css("min-height",$( window ).height());
 
     $(window).scrollTop(topGutterHeight);
-    
+
+    $(window).scroll(update_page_properties);
+
     $(window).resize(function(){
       $(".site").eq(0).css("min-height",$( window ).height());
+      update_page_properties()
     });
-    
-    $(window).scroll(function(){
-      scrollPos = $(window).scrollTop();
-
-      if (0 >= scrollPos) {
-        $("body").eq(0).addClass('is-top');
-        $("body").eq(0).removeClass('is-not-top');
-      } else {
-        $("body").eq(0).removeClass('is-top');
-        $("body").eq(0).addClass('is-not-top');
-      }
-
-      setTimeout(function(){
-        scrollPos = $(window).scrollTop();
         
-        if (scrollPos < (topGutterHeight - 31)) {
-          $("body").eq(0).addClass('is-top-gutter-shown');
-          $("body").eq(0).removeClass('is-not-top-gutter-shown');
-        } else {
-          $("body").eq(0).removeClass('is-top-gutter-shown');
-          $("body").eq(0).addClass('is-not-top-gutter-shown');
-        }
-      }, 500);
-    });
-    
-    $(window).scrollTop(parseInt($("body").eq(0).css("padding-top")));
-    
   });
 })(jQuery);
