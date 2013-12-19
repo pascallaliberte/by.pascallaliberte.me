0a06643def07e152dfcdc05f01ab3db389588691
Pascal Laliberté <pascal.laliberte@gmail.com>
2013-12-18 22:53:34 -0500

    Enabled the toggle of .subscribe-follow - Toggled the button's text to Done when .subscribe-follow is shown

--- a/js/main.js
+++ a/js/main.js
@@ -94,6 +94,30 @@
         });
   }
   
+  init_subscribe_follow = function() {
+    $(".subscribe-follow-link").each(function(){
+      var width_px = parseInt($(this).width());
+      var font_size_px = parseInt($(this).css('font-size'));
+      var width_em = (width_px / font_size_px) + 'em';
+
+      // lock the width
+      $(this).css("width", width_em);
+    });
+
+    $(".subscribe-follow-link").click(function(){
+      $("body").eq(0).toggleClass("is-subscribe-follow");
+      
+      // toggle the text
+      if($(this).data('toggle-text')) {
+        current_html = $(this).html();
+        $(this).html($(this).data('toggle-text'));
+        $(this).data('toggle-text', current_html);
+      }
+      
+      event.preventDefault();
+    });    
+  }
+  
   $(document).ready(function(){
     $("body").eq(0).addClass('js');
     
@@ -118,5 +142,6 @@
     });
         
     init_gallery();
+    init_subscribe_follow();
   });
 })(jQuery);
