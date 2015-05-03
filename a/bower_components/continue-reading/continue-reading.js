301e2c5c5da31bf2d74bfba54a39745d96a8a7db
Pascal Laliberté <pascal.laliberte@gmail.com>
2015-05-02 22:55:39 -0400

    Update continue-reading to 0.3.0 - Repair responsive images - Improve experience on iPad, iPhone

--- a/bower_components/continue-reading/continue-reading.js
+++ a/bower_components/continue-reading/continue-reading.js
@@ -1,9 +1,31 @@
+/* $.inlineStyle() credit @james_allardice
+ * see http://stackoverflow.com/a/11306872
+ */
+(function ($) {
+  $.fn.inlineStyle = function (prop) {
+    return this.prop("style")[$.camelCase(prop)];
+  };
+}(jQuery));
+
+/*
+ * $.continueReading()
+ */
 (function ( $ ) {
 
   var languages = {
     'en': 'continue-reading',
     'fr': 'continuer-lecture'
   };
+
+  var exemptFromViewPortHeightOffset = [
+    'CriOS' // chrome for iOS
+  ];
+
+  var bottomBarHeightsToCancel = {
+    '(iPhone|iPod)[^\)]*OS (7|8)': {
+      '(orientation: portrait)': 44
+    }
+  }
 
   var defaultLang = 'en';
 
@@ -23,16 +45,29 @@
         localStorage.removeItem('continueReadingScrollOffset');
       }
 
+      // adjust the offset if the viewport height has changed after the click
+      // e.g. iOS Safari's URL bar expanded on iPad
+      if (localStorage['viewportDimensions']) {
+        previousViewportHeight = JSON.parse(localStorage['viewportDimensions']).height;
+        localStorage.removeItem('viewportDimensions');
+        currentViewportHeight = window.innerHeight;
+        deltaViewportHeight = previousViewportHeight - currentViewportHeight;
+
+        if (!isUAExemptFromViewPortHeightOffset(navigator.userAgent, exemptFromViewPortHeightOffset)) {
+          // cancel the effect of a bottom bar on the viewport height if changed
+          if (0 != deltaViewportHeight) {
+            deltaViewportHeight -= getUABottomBarHeightToCancel(navigator.userAgent, bottomBarHeightsToCancel);
+          }
+
+          // modify the offset with the difference in viewport height
+          continueReadingScrollOffset = parseFloat(continueReadingScrollOffset) + parseFloat(deltaViewportHeight);
+        }
+      }
+
       // set the width and height of images and other items before they're done
       // loading so that when they're done, they don't push down the
       // continue-reading position
-      if (localStorage['stuffToPreSizeAboveContinueReading']) {
-        stuffToPresize = JSON.parse(localStorage['stuffToPreSizeAboveContinueReading']);
-        localStorage.removeItem('stuffToPreSizeAboveContinueReading');
-        for(selector in stuffToPresize) {
-          $(selector).innerWidth(stuffToPresize[selector].width).innerHeight(stuffToPresize[selector].height);
-        }
-      }
+      preSizeStuffAboveContinueReading();
 
       continueReadingScrollPos = 0;
       if (1 <= $('#' + continueReading).size()) {
@@ -51,8 +86,18 @@
     return this.each(function() {
       $(this).find("a[href$='#" + continueReading + "']").click(function( event ) {
         event.preventDefault();
+        // save viewport dimensions to detect if a change occured when clicking
+        // the link, e.g. iOS Safari's URL bar expanded
+        localStorage['viewportDimensions'] = JSON.stringify({
+          width: window.innerWidth,
+          height: window.innerHeight
+        });
+
         continueReadingScrollOffset = this.getBoundingClientRect().top * -1;
         localStorage['continueReadingScrollOffset'] = continueReadingScrollOffset;
+
+        // detect the dimensions of images and iframes above the
+        // continue-reading link that could affect the offset to shoot for
         localStorage['stuffToPreSizeAboveContinueReading'] = JSON.stringify(findStuffToPreSizeAbove(this));
         document.location = this.href;
       });
@@ -99,6 +144,73 @@
 
     return stuffToPreSize;
 
+  }
+
+  function preSizeStuffAboveContinueReading () {
+    if (localStorage['stuffToPreSizeAboveContinueReading']) {
+      stuffToPresize = JSON.parse(localStorage['stuffToPreSizeAboveContinueReading']);
+      for(selector in stuffToPresize) {
+        el = $(selector);
+        el.data('original-width', el.inlineStyle('width'));
+        el.data('original-height', el.inlineStyle('height'));
+        el.css('width', stuffToPresize[selector].width);
+        el.css('height', stuffToPresize[selector].height);
+      }
+
+      $(window).on('load', cancelPreSizeStuffAboveContinueReading);
+      $(window).on('resize', cancelPreSizeStuffAboveContinueReading);
+    }
+  }
+
+  function cancelPreSizeStuffAboveContinueReading () {
+    if (localStorage['stuffToPreSizeAboveContinueReading']) {
+      stuffToPresize = JSON.parse(localStorage['stuffToPreSizeAboveContinueReading']);
+      for(selector in stuffToPresize) {
+        el = $(selector);
+        el.css('width', el.data('original-width'));
+        el.css('height', el.data('original-height'));
+        el.removeData('original-width');
+        el.removeData('original-height');
+
+        if (el.attr('style') == '') {
+          el.removeAttr('style');
+        }
+      }
+
+      $(window).off('load', cancelPreSizeStuffAboveContinueReading);
+      $(window).off('resize', cancelPreSizeStuffAboveContinueReading);
+
+      localStorage.removeItem('stuffToPreSizeAboveContinueReading');
+    }
+  }
+
+  function isUAExemptFromViewPortHeightOffset(ua, exemptFromViewPortHeightOffset) {
+    for (uaPattern of exemptFromViewPortHeightOffset) {
+      return (ua.match(new RegExp(uaPattern, "gi")));
+    }
+    return false;
+  }
+
+  function getUABottomBarHeightToCancel(ua, bottomBarHeightsToCancel) {
+    var bottomBarHeight = 0;
+    var uaMatched = null;
+
+    for (uaPattern in bottomBarHeightsToCancel) {
+      if (ua.match(new RegExp(uaPattern, "gi"))) {
+        uaMatched = uaPattern;
+      }
+    }
+
+    if (uaMatched) {
+      for (mediaQuery in bottomBarHeightsToCancel[uaMatched]) {
+        var mq = window.matchMedia(mediaQuery);
+        if (mq.matches) {
+          bottomBarHeight = bottomBarHeightsToCancel[uaMatched][mediaQuery];
+        }
+      }
+    }
+
+    return bottomBarHeight;
   }
 
 }( jQuery ));
